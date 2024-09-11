17210ed624c75005564cc219c052ae72796935c0
Chandana <155734407+Chandanayeragadindla@users.noreply.github.com>
2024-09-11 12:28:47 +0530

    primeNum.java

--- PS_SHEET1/primeNum.java
+++ PS_SHEET1/primeNum.java
@@ -1,3 +1,15 @@
+//question//
+Checking for Prime Numbers
+Difficulty: Easy
+Topics: Basic Programming, Number Theory
+Description: Write a program to determine if a number is prime.
+Example:
+Input: number = 7
+Output: Prime
+Explanation: 7 has no divisors other than 1 and itself, so it is a prime number.
+
+
+//code//
 import java.util.Scanner;
 
 public class primeNum {
